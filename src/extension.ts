da024e81f38649709e4d2e7c507a49f04ee05400
David Nicholson <me@djnicholson.com>
2019-10-06 19:31:59 +0100

    add a refresh button

--- src/extension.ts
+++ src/extension.ts
@@ -8,14 +8,20 @@ export function activate(context: vscode.ExtensionContext) {
 
 	const rpcConnection = new NeoRpcConnection();
 
+	const rpcServerExplorer = new RpcServerExplorer();
+
 	const openTrackerCommand = vscode.commands.registerCommand('extension.openTracker', (url: string) => {
-		console.log('extension.openTracker', url);
 		const panel = new NeoTrackerPanel(context.extensionPath, rpcConnection, context.subscriptions);
 	});
 
-	const serverExplorer = vscode.window.registerTreeDataProvider('extension.rpcServerExplorer', new RpcServerExplorer());
+	const refreshServersCommand = vscode.commands.registerCommand('extension.refreshServers', () => {
+		rpcServerExplorer.refresh();
+	});
+
+	const serverExplorer = vscode.window.registerTreeDataProvider('extension.rpcServerExplorer', rpcServerExplorer);
 
 	context.subscriptions.push(openTrackerCommand);
+	context.subscriptions.push(refreshServersCommand);
 	context.subscriptions.push(serverExplorer);
 }
 
