b1e3a37100556a304bea114b224e22ac0aaf170a
David Nicholson <djnicholson@users.noreply.github.com>
2019-12-03 16:22:05 +0000

    Use Neo Express endpoint to get list of populated blocks (instead of calculating locally) (#22)
    * perf improvements for when RPC server not running
    
    * Use Neo Express endpoint to get list of populated blocks (instead of calculating locally)
    
    * unsed import
    
    * chainidentifier no longer used

--- src/rpcConnectionPool.ts
+++ src/rpcConnectionPool.ts
@@ -1,18 +1,16 @@
-import { Memento } from "vscode";
-
 import { INeoRpcConnection, NeoRpcConnection } from "./neoRpcConnection";
 
 export class RpcConnectionPool {
 
     private readonly connections: Map<string, INeoRpcConnection> = new Map<string, INeoRpcConnection>();
 
-    constructor(private readonly globalState: Memento) {
+    constructor() {
     }
 
     public getConnection(rpcUrl: string): INeoRpcConnection {
         let pooledConnection = this.connections.get(rpcUrl);
         if (!pooledConnection) {
-            pooledConnection = new NeoRpcConnection(rpcUrl, this.globalState);
+            pooledConnection = new NeoRpcConnection(rpcUrl);
             this.connections.set(rpcUrl, pooledConnection);
         }
 
