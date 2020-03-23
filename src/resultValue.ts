1f4eec228f6deaf63b604347e239f533d171f9a7
David Nicholson <djnicholson@users.noreply.github.com>
2020-03-23 21:58:41 +0000

    Correctly interpret integer arguments/return values that are less than 16 (#125)
    * Correctly interpret integer arguments/return values that are less than 16
    
    * Fixes #5

--- src/resultValue.ts
+++ src/resultValue.ts
@@ -1,30 +1,47 @@
 const bs58check = require('bs58check');
 
 export class ResultValue {
+    
     public readonly asInteger: string;
     public readonly asByteArray: string;
     public readonly asString: string;
     public readonly asAddress: string;
+
     constructor(result: any) {
         let value = result.value;
         if ((value !== null) && (value !== undefined)) {
-            if (value.length === 0) {
-                value = '00';
-            }
-            this.asByteArray = '0x' + value;
-            const buffer = Buffer.from(value, 'hex');
-            this.asString = buffer.toString();
+            this.asString = '';
             this.asAddress = '';
-            if (value.length === 42) {
-                this.asAddress = bs58check.encode(buffer);
+            this.asInteger = '';
+            if (result.type === 'Integer') {
+                this.asInteger = value;
+                this.asByteArray = '0x' + ResultValue.addLeadingZeroes(parseInt(value).toString(16));
+            } else {
+                const buffer = Buffer.from(value, 'hex');
+                this.asByteArray = '0x' + buffer.toString('hex');
+                this.asString = buffer.toString();
+                if (value.length === 42) {
+                    this.asAddress = bs58check.encode(buffer);
+                }
+                if (value.length > 0) {
+                    this.asInteger = BigInt(this.asByteArray).toString();
+                }
             }
-            buffer.reverse();
-            this.asInteger = BigInt('0x' + buffer.toString('hex')).toString();
         } else {
             this.asInteger = '0';
             this.asByteArray = '(empty)';
             this.asString = '(empty)';
             this.asAddress = '(empty)';
+        }
+    }
+
+    public static addLeadingZeroes(input: string) {
+        if (input.length === 0) {
+            return '00';
+        } else if ((input.length % 2) === 1) {
+            return '0' + input;
+        } else {
+            return input;
         }
     }
 }
